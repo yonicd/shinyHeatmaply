081b63348bb5ac446647f45a62fe40b28a230743
Tal Galili <tal.galili@gmail.com>
2017-03-22 13:43:35 +0200

    1000px is too much.

--- server.r
+++ server.r
@@ -210,7 +210,7 @@ observeEvent({interactiveHeatmap()},{
   # isolate({h<-interactiveHeatmap()})
   h<-interactiveHeatmap()
   h$width='100%'
-  h$height='1000px'
+  h$height='600px'
   s<-tags$div(style="position: relative; bottom: 5px;",
               #tags$p(
                 tags$em('This heatmap visualization was created using',
