ff473dbe126e5b73045a79d529f22148d15cfb01
Tal Galili <tal.galili@gmail.com>
2017-03-24 19:11:26 +0300

    Should fix:
    Running examples in 'shinyHeatmaply-Ex.R' failed
    The error most likely occurred in:

--- R/launch_heatmaply.R
+++ R/launch_heatmaply.R
@@ -4,12 +4,14 @@
 #'@param plotHeight numeric that sets the height of the plot output (default 800px)
 #'@param viewerType character of the viewer to be used to launch the app to c('paneViewer','dialogViewer','browserViewer')
 #'@examples
+#' \dontrun{
 #'if(interactive()){
 #'data(mtcars)
 #'launch_heatmaply(mtcars)
 #'
 #'data(iris)
 #'launch_heatmaply(list('Example1'=mtcars,'Example2'=iris))
+#'}
 #'}
 #'@export
 launch_heatmaply=function(obj,plotHeight=800,viewerType='paneViewer'){
