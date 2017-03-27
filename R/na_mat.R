a0c5142b09269f3a58ac21af77fda6e329206c4b
Tal Galili <tal.galili@gmail.com>
2017-03-27 10:48:32 +0300

    Removing na_mat (which was replaced by heatmaply::is.na10)

--- R/na_mat.R
+++
@@ -1,9 +0,0 @@
-#'@title na_mat
-#'@description convert to numeric if there is NA in object
-#'@param x object
-#'@keywords internal
-#'@export
-#'@import dplyr
-na_mat <- function(x,...) {
-  x%>%is.na()%>%class_to("numeric") 
-}
