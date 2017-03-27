51b8c2c4e9d4e49adaa83bb1c9d2be777d7c29ad
Tal Galili <tal.galili@gmail.com>
2017-03-27 10:45:19 +0300

    Fix:
    checking dependencies in R code ... NOTE
    Package in Depends field not imported from: 'heatmaply'
      These packages need to be imported from (in the NAMESPACE file)
      for when this namespace is loaded but not attached.

---
+++ R/zzz.R
@@ -0,0 +1,31 @@
+#
+
+
+#' @import heatmaply
+NULL
+
+
+
+# when a function is renamed, its document in man must be removed - otherwise it may cause problems with the built check (it will try to run the code in the example, and will fail.)
+# When all is done, run:
+# library(devtools)
+# check()
+# browseURL(tempdir())
+### http://www.rstudio.com/ide/docs/packages/build_options
+#
+# check(build_args="--no-build-vignettes --no-manual", args = "--no-examples --no-build-vignettes --no-manual",  cran = FALSE, cleanup = FALSE)
+# check(build_args="--no-build-vignettes --no-manual", args = "--no-build-vignettes --no-manual",  cran = FALSE, cleanup = FALSE)
+# check(build_args="--no-build-vignettes ", args = "--no-build-vignettes",  cran = FALSE, cleanup = FALSE)
+# devtools::check(args="--as-cran")
+# devtools::check("C:/Dropbox/aaaa good R code/AA - My packages/heatmaply", args="--as-cran")
+#                 Thanks to: http://stackoverflow.com/questions/10017702/r-cmd-check-options-for-more-rigorous-testing-2-15-0
+
+
+# shell('git log --graph --stat --date=short --pretty=format:"%ad(%an) %s |%h" > ChangeLog', intern = TRUE)
+# file.copy("NEWS", "NEWS.md",overwrite = TRUE)
+# devtools::build_win(version="R-release")
+# devtools::build_win(version="R-devel")
+# release()
+
+
+
